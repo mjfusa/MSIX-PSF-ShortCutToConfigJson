9b6a38f762c3062c4e1e3c2b41643bd64a4fa259
Mike Francis <mike.francis@live.com>
2021-12-12 13:25:27 -0800

    Started with manifest update

--- ShortCutToConfig.ps1
+++ ShortCutToConfig.ps1
@@ -16,7 +16,6 @@ $appsArray = [System.Collections.ArrayList]@()
 
 # Get AppIds from manifest
 [xml]$manifest = get-content "AppxManifest.xml"
-#$manifest.Package.Identity.Version = "$env:NBGV_SimpleVersion.0"
 $apps= $manifest.Package.Applications.Application
 foreach ($app in $apps) {
     $object = new-object psobject -Property $applications
@@ -28,11 +27,39 @@ foreach ($app in $apps) {
 # https://www.alexandrumarin.com/add-shortcut-arguments-in-msix-with-psf/
 $sh = New-Object -ComObject WScript.Shell
 $files=Get-ChildItem *.lnk
+$found=$false;
+$foundArray = [System.Collections.ArrayList]@()
 foreach ($file in $files) {
     $ManifestExe = Split-Path $app.Executable -Leaf
     $ShortcutExe = Split-Path $sh.CreateShortcut($file).TargetPath  -Leaf
-    $res1 = ($appsArray | where { (Split-Path $_.executable -Leaf) -eq (Split-Path $sh.CreateShortcut($file).TargetPath -Leaf)})
-    if ($res1 -ne $null) {
+    #$ShortcutExe
+    $shResult = $sh.CreateShortcut($file);
+    $shResult 
+    Write-Host("---")
+    $foundPath='';
+    # Search for ShortCutExe in appArray, add addtional Application node to AppxManifest if exe appears in multiple lnks.
+    # MPT only creates one Application node per exe.
+    $res1 = ($appsArray | Where-Object { (Split-Path $_.executable -Leaf) -eq (Split-Path $sh.CreateShortcut($file).TargetPath -Leaf)})
+    if ($null -ne $res1) {
+        $findRes =($foundArray | Where-Object {((Split-Path $sh.CreateShortcut($file).TargetPath -Leaf))})
+        if ($null -eq $findRes) {
+            $index = [array]::IndexOf($appsArray, $res1)
+            # $t = apps[$index];
+            #$t = $manifest.CreateElement("Application");
+            #$t.SetAttribute("Id",$apps[$index].Id);
+            #$manifest.Package.Applications.AppendChild($t);
+            $t = $manifest.Package.Applications.Application[$index].Clone();
+            $manifest.Package.Applications.AppendChild($t);
+            
+
+        } else {
+            $foundArray.Add((Split-Path $sh.CreateShortcut($file).TargetPath -Leaf))
+        }
+        if ($found -eq $true) {
+            $i = $appsArray.Add($res1);
+            $res1 = res$appsArray[$i];
+            $found=$false;
+        } 
         $res1.executable = $sh.CreateShortcut($file).TargetPath  
         $res1.arguments = $sh.CreateShortcut($file).Arguments
     }
